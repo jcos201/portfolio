f1ff30e8699333191d891879aca4525a4566794b
Jorge L. Cossi <jcos201@gmail.com>
2021-04-09 22:17:43 -0400

    Updates footer with social media icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 import * as styles from './Footer.module.scss'
+import gatsbyJS from '../../../static/gatsby.ico'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = (props) => (
     <footer className={styles.Footer}>
+        <p className={styles.topRow}>
+            <FontAwesomeIcon className={styles.icon} icon={faLinkedin} />
+            <FontAwesomeIcon className={styles.icon} icon={faGithub} />
+            <FontAwesomeIcon className={styles.icon} icon={faTwitter} />
+        </p>
         <p>
-            Copyright &copy; All Rights Reserved
+            Built using <img src={gatsbyJS} className={styles.gatsbyLogo} alt="GatsbyJS logo"/> by {props.author}
+            {' '} &copy; All Rights Reserved
             {' '}{ new Date().getFullYear() } Cossi.Dev
-            {' '} {props.author}
         </p>
     </footer>
 );
