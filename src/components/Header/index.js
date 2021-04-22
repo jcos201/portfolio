100c7c055f5d2ddaa5a5df820551f6894e1f2535
Jorge L. Cossi <jcos201@gmail.com>
2021-04-22 05:42:59 -0400

    collapses burger menu after onClick

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,56 +5,33 @@ import { Nav, Navbar } from 'react-bootstrap';
 import logo from "../../../static/my_logo.png";
 
 
-const navHeader = {
-    width: '100%',
-    backgroundColor: '#f7f9fb',    
-}
-
-const linkFont = {
-    textAlign: 'right',
-}
-
 const myLogo = {
     margin: '0',
 }
 
+const navText = {
+    textAlign: 'right',
+}
+
 const Header = (props) => (
-    <header  style={navHeader} class="sticky-top">  
-            <Navbar expand="lg" variant="light">
-                <Navbar.Brand href="/"><img src={logo} style={myLogo} alt="My Logo"/></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link as={Link} style={linkFont} to="/">Home</Nav.Link>
-                        <Nav.Link as={Link} style={linkFont} to="/about">About</Nav.Link>
-                        <Nav.Link as={Link} style={linkFont} to="/projects">Projects</Nav.Link>
-                        <Nav.Link as={Link} style={linkFont} to="/contact">Contact</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-    </header>
+    // Collapses Navbar to burger menu on screens smaller than 992px
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light" fixed="top">
+        <Navbar.Brand href="/">
+            <img src={logo} style={myLogo} alt="My Logo"/>
+        </Navbar.Brand>
+        {/* These items will be part of burger menu on smaller screens */}
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+            <Nav >
+                {/* Need to have href in order to toggle burger menu closed after onClick */}
+                <Nav.Link href="/" style={navText}>Home</Nav.Link>
+                <Nav.Link as={Link} to="/about" href="/about" style={navText}>About</Nav.Link>
+                <Nav.Link as={Link} to="/projects" href="/projects" style={navText}>Projects</Nav.Link>
+                <Nav.Link as={Link} to="/contact" href="/contact" style={navText}>Contact</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+
 );
 
 export default Header;
-
-/*
- 
-        <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-                <h1 class="navbar-brand"></h1>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon">dd</span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item"> <Link class="nav-link active" aria-current="page" style={navLink} to="/">Home</Link> </li>
-                        <li class="nav-item"> <Link class="nav-link active" aria-current="page" style={navLink} to="/about">About</Link> </li>
-                        <li class="nav-item"> <Link class="nav-link active" aria-current="page" style={navLink} to="/projects">Projects</Link> </li>
-                        <li class="nav-item"> <Link class="nav-link active" aria-current="page" style={navLink} to="/articles">Articles</Link> </li>
-                        <li class="nav-item"> <Link class="nav-link active" aria-current="page" style={navLink} to="/contact">Contact</Link> </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    
-*/
